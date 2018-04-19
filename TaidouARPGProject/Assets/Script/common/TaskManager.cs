6df01110280d1b417af71d0de7cf11294e29fc6e
fthirtypro <517017035@qq.com>
2018-04-19 16:50:39 +0800

    任务内如序列话

---
+++ TaidouARPGProject/Assets/Script/common/TaskManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TaskManager : MonoBehaviour {
+
+public TextAsset taskinfoText;
+private ArrayList taskList=new ArrayList();
+
+/// <summary>
+/// 初始化任务信息列表
+/// </summary>
+ public void InitTask()
+ {
+	 string[] taskArray = taskinfoText.ToString().Split('\n');
+	 foreach(string strArr in taskArray)
+	 {
+		 string[] proArray = strArr.Split('|');
+		 Task task = new Task();
+		 task.Id =int.Parse(proArray[0]);
+			switch(proArray[1])
+			{
+				case "Main":
+				task.Tasktype = Task.TaskType.Main;
+				break;
+				case "Reward":
+				task.Tasktype =Task.TaskType.Reward;
+				break;
+				case "Daily":
+				task.Tasktype =Task.TaskType.Daily;
+				break;
+				
+			}
+
+		 task.Name = proArray[2];
+		 task.Icon = proArray[3];
+		 task.Des  = proArray[4];
+		 task.Coin = int.Parse(proArray[5]);
+		 task.Diamond =int.Parse(proArray[6]);
+		 task.TalkNpc =proArray[7];
+		 task.IdNpc =int.Parse (proArray[8]);
+		 task.IdTranscript = int.Parse(proArray[9]);
+			taskList.Add(task);
+
+	 }
+ }
+
+}
---
+++ TaidouARPGProject/Assets/Script/mainmenu/Task/Task.cs
@@ -0,0 +1,162 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Task {
+
+	public enum TaskType {
+		Main,
+		Reward,
+		Daily
+	}
+
+	public enum TaskProgress {
+		NoStart,
+		Accept,
+		Complete,
+		ReWard
+	}
+
+	private int id;
+
+	
+
+    private TaskType taskType;
+	private string name;
+	private string icon;
+	private string des;
+	private int coin;
+	private int diamond;
+	private string talkNpc; //对话NPC 内容
+	private int idNpc; //npc的id
+	private int idTranscript; //副本id
+	private TaskProgress taskProgress = TaskProgress.NoStart;
+
+
+public int Id {
+		get {
+			return id;
+		}
+		set { id = value; }
+	}
+
+    public TaskType Tasktype
+    {
+        get
+        {
+            return taskType;
+        }
+
+        set
+        {
+            taskType = value;
+        }
+    }
+
+    public string Name
+    {
+        get
+        {
+            return name;
+        }
+
+        set
+        {
+            name = value;
+        }
+    }
+
+    public string Icon
+    {
+        get
+        {
+            return icon;
+        }
+
+        set
+        {
+            icon = value;
+        }
+    }
+
+    public string Des
+    {
+        get
+        {
+            return des;
+        }
+
+        set
+        {
+            des = value;
+        }
+    }
+
+    public int Coin
+    {
+        get
+        {
+            return coin;
+        }
+
+        set
+        {
+            coin = value;
+        }
+    }
+
+    public int Diamond
+    {
+        get
+        {
+            return diamond;
+        }
+
+        set
+        {
+            diamond = value;
+        }
+    }
+
+    public string TalkNpc
+    {
+        get
+        {
+            return talkNpc;
+        }
+
+        set
+        {
+            talkNpc = value;
+        }
+    }
+
+    public int IdNpc
+    {
+        get
+        {
+            return idNpc;
+        }
+
+        set
+        {
+            idNpc = value;
+        }
+    }
+
+    public int IdTranscript
+    {
+        get
+        {
+            return idTranscript;
+        }
+
+        set
+        {
+            idTranscript = value;
+        }
+    }
+
+	//构造方法完成；
+	///任务类的完成；
+}
