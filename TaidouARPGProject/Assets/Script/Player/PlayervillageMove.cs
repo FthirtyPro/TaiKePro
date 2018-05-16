c95ecb869e4f7e7b2ba88ee45d79b7b6503b796f
FTHIRYTPRO\luant <fthirtypro@163.com>
2018-05-16 22:30:11 +0800

    人物寻路1/3；

---
+++ TaidouARPGProject/Assets/Script/Player/PlayervillageMove.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayervillageMove : MonoBehaviour {
+
+	public float velocity;
+
+	void Update()
+	{
+		Rigidbody rig = GetComponent<Rigidbody>();
+		float h = Input.GetAxis("Horizontal");
+		float v =Input.GetAxis("Vertical");
+		Vector3 vel = rig.velocity;
+		rig.velocity= new Vector3(-h*velocity,vel.y,-v*velocity);
+
+
+        if (Mathf.Abs(h) > 0.05f || Mathf.Abs(v) > 0.05f)
+        {
+            transform.rotation = Quaternion.LookRotation(new Vector3(-h, 0, -v));
+        }
+        
+      
+	}
+
+}
--- TaidouARPGProject/Assets/Script/common/FollowTarget.cs
+++ TaidouARPGProject/Assets/Script/common/FollowTarget.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class FollowTarget : MonoBehaviour {
 
     public Vector3 offset;
-    private Transform playerBip;
+    public Transform playerBip;
 
 	// Use this for initialization
 	void Start () {
