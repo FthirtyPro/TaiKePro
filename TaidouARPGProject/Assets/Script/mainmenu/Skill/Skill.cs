84aa04199bcb9ec7a8a0d0b40f0a5b5a50984e82
FTHIRYTPRO\luant <fthirtypro@163.com>
2018-05-30 23:24:57 +0800

    skill基础类

---
+++ TaidouARPGProject/Assets/Script/mainmenu/Skill/Skill.cs
@@ -0,0 +1,150 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skill
+{
+
+    public enum PosType
+    {
+        Basic,
+        One,
+        Two,
+        Three
+    }
+
+    public enum SkillType
+    {
+        Basic,
+        Skill
+    }
+
+
+    private int id;
+    private string name;
+    private string icon;
+    private int damage;
+    private int cold;
+    private int level = 1;
+    private PlayerType playerType;
+    private SkillType skillType;
+    private PosType posType;
+
+    public int Id
+    {
+        get
+        {
+            return id;
+        }
+
+        set
+        {
+            id = value;
+        }
+    }
+
+    public string Name
+    {
+        get
+        {
+            return name;
+        }
+
+        set
+        {
+            name = value;
+        }
+    }
+
+    public string Icon
+    {
+        get
+        {
+            return icon;
+        }
+
+        set
+        {
+            icon = value;
+        }
+    }
+
+    public int Damage
+    {
+        get
+        {
+            return damage;
+        }
+
+        set
+        {
+            damage = value;
+        }
+    }
+
+    public int Cold
+    {
+        get
+        {
+            return cold;
+        }
+
+        set
+        {
+            cold = value;
+        }
+    }
+
+    public int Level
+    {
+        get
+        {
+            return level;
+        }
+
+        set
+        {
+            level = value;
+        }
+    }
+
+    public PlayerType PlayerType
+    {
+        get
+        {
+            return playerType;
+        }
+
+        set
+        {
+            playerType = value;
+        }
+    }
+
+    public SkillType Skilltype
+    {
+        get
+        {
+            return skillType;
+        }
+
+        set
+        {
+            skillType = value;
+        }
+    }
+
+    public PosType Postype
+    {
+        get
+        {
+            return posType;
+        }
+
+        set
+        {
+            posType = value;
+        }
+    }
+}
+
---
+++ TaidouARPGProject/Assets/Script/mainmenu/Skill/SkillManager.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillManager : MonoBehaviour {
+
+    public TextAsset skill;
+    public static SkillManager _instance;
+
+    private void Awake()
+    {
+        _instance = this;
+        ///readSkillText();
+    }
+    private void Start()
+    {
+        readSkillText();
+    }
+
+    public void readSkillText()
+    {
+        string sr = skill.ToString();
+        string[] str = sr.Split('\n');
+
+        foreach(string arr in str)
+        {
+            string[] arrstr = arr.Split('|');
+
+            Skill skillarr = new Skill();
+            skillarr.Id = int.Parse(arrstr[0]);
+            skillarr.Name = arrstr[1];
+            skillarr.Icon = arrstr[2];
+            switch(arrstr[3])
+            {
+                case "Warrior":
+                    skillarr.PlayerType = PlayerType.Warrior;
+                    break;
+
+                case "FemaleAssassin":
+                    skillarr.PlayerType = PlayerType.FemaleAssassin;
+                    break;
+
+
+            }
+            switch (arrstr[4])
+            {
+                case "Basic":
+                    skillarr.Skilltype =Skill.SkillType.Basic;
+                    break;
+
+                case "Skill":
+                    skillarr.Skilltype = Skill.SkillType.Skill;
+
+                    break;
+
+            }
+
+            switch (arrstr[5])
+            {
+                case "Basic":
+                    skillarr.Postype = Skill.PosType.Basic;
+                    break;
+
+                case "One":
+                    skillarr.Postype = Skill.PosType.One;
+
+                    break;
+
+                case "Two":
+                    skillarr.Skilltype = Skill.SkillType.Skill;
+
+                    break;
+
+                case "Three":
+                    skillarr.Skilltype = Skill.SkillType.Skill;
+
+                    break;
+
+            }
+
+            skillarr.Cold = int.Parse(arrstr[6]);
+            skillarr.Damage = int.Parse(arrstr[7]);
+
+
+
+
+        }
+    }
+
+
+}
