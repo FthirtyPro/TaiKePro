54bb59dd970fac0aad21cdbe21d26b488cbbca08
fthirtypro <fthirtypro@163.com>
2018-06-11 20:00:03 +0800

    no message

---
+++ TaidouARPGProject/Assets/PlayEffect.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayEffect : MonoBehaviour {
+
+private Renderer[] renderArray;
+private NcCurveAnimation[]  nccurveArrey;
+
+void  Update()
+{
+   if(Input.GetMouseButton(0))
+   Show();
+}
+
+void Start()
+{
+    renderArray = this.GetComponentsInChildren<Renderer>();
+    nccurveArrey = this.GetComponentsInChildren<NcCurveAnimation>();
+}
+
+public void Show()
+{
+    foreach(Renderer  render in renderArray)
+    {
+        render.enabled =true;
+    }
+    foreach(NcCurveAnimation  Ncc in nccurveArrey)
+    {
+        Ncc.ResetAnimation();
+    }
+}
+
+
+
+}
